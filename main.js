a94b60f95ec0cb92ebe64324585cb5b5a71acc7a
Yuriy Dybskiy <yuriy@dybskiy.com>
2014-04-15 13:23:34 -0700

    Basic constructor

--- main.js
+++ main.js
@@ -1,49 +1,57 @@
-var Point = Backbone.Model.extend({
-  initialize: function() {
+// var Point = Backbone.Model.extend({
+//   initialize: function() {
 
-    this.on('change:count', function() {
-      console.log(JSON.stringify(this));
+//     this.on('change:count', function() {
+//       console.log(JSON.stringify(this));
+//     });
+//   },
+
+//   defaults: {
+//     time: Date.now(),
+//     count: 0
+//   }
+
+// });
+
+// var Graph = Backbone.Collection.extend({
+//   model: Point,
+
+//   url: 'https://dybskiy.cloudant.com/car2go-march/_design/stats/_view/byCity?group_level=6&reduce=true&startkey=[%22Vancouver%22]&endkey=[%22Washington%20DC%22]',
+//   dataType: "jsonp",
+
+//   initialize: function() {
+//     this.on('add', function(point) {
+//       console.log('Added ' + JSON.stringify(point));
+//     });
+//   }
+// });
+
+// var graph = new Graph();
+// var point = new Point();
+
+// // graph.fetch();
+
+// var points  = $.ajax({
+//    url: 'https://dybskiy.cloudant.com/car2go-march/_design/stats/_view/byCity?group_level=6&reduce=true&startkey=[%22Vancouver%22]&endkey=[%22Washington%20DC%22]',
+
+//    crossDomain : true
+
+// }).done(function(data) {
+//   console.log(data);
+// });
+
+
+var Person = function (age) {
+  this.age = age || 18;
+  this.backpack = [];
+  this.addItemToBackpack = function (item) {
+    this.backpack.push(item);
+  };
+
+  this.iterateMe = function () {
+    this.backpack.forEach(function (item) {
+      console.log(item);
+      console.log(this);
     });
-  },
-
-  defaults: {
-    time: Date.now(),
-    count: 0
   }
-
-});
-
-var Graph = Backbone.Collection.extend({
-  model: Point,
-
-  url: 'https://dybskiy.cloudant.com/car2go-march/_design/stats/_view/byCity?group_level=6&reduce=true&startkey=[%22Vancouver%22]&endkey=[%22Washington%20DC%22]',
-  dataType: "jsonp",
-
-  initialize: function() {
-    this.on('add', function(point) {
-      console.log('Added ' + JSON.stringify(point));
-    });
-  }
-});
-
-var graph = new Graph();
-var point = new Point();
-
-// graph.fetch();
-
-
-var points  = $.ajax({
-   url: 'https://dybskiy.cloudant.com/car2go-march/_design/stats/_view/byCity?group_level=6&reduce=true&startkey=[%22Vancouver%22]&endkey=[%22Washington%20DC%22]',
-
-   crossDomain : true
-
-}).done(function(data) {
-  console.log(data);
-});
-
-
-
-
-
-
-
+}
